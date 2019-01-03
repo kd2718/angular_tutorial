6724089d9db0a9e9fe9fef9719a57858fed28196
kd2718 <korydonati@gmail.com>
2019-01-02 22:02:55 -0800

    finished messageing

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,6 +3,8 @@ import { Injectable } from '@angular/core';
 import {Hero} from './hero';
 import {HEROES} from './mock-heroes';
 
+import {MessageService} from './message.service';
+
 import {Observable, of} from 'rxjs';
 
 @Injectable({
@@ -11,8 +13,10 @@ import {Observable, of} from 'rxjs';
 export class HeroService {
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send message after getting heroes
+    this.messageService.add('HeroService: Fetched Heroes');
     return of(HEROES);
   }
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 }
