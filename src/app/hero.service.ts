e23fb5af314fb75787b627bad6f1cf03e625a3f9
kd2718 <korydonati@gmail.com>
2019-01-06 13:52:49 -0800

    modified heroservice to use faked url

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,27 +1,63 @@
 import { Injectable } from '@angular/core';
 
 import {Hero} from './hero';
-import {HEROES} from './mock-heroes';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {MessageService} from './message.service';
 
 import {Observable, of} from 'rxjs';
+
+import {catchError, map, tap} from 'rxjs/operators';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class HeroService {
 
+  private heroesUrl = 'api/heroes'; // Url to web API
+
   getHeroes(): Observable<Hero[]> {
     // TODO: send message after getting heroes
     this.messageService.add('HeroService: Fetched Heroes');
-    return of(HEROES);
+    return this.http.get<Hero[]>(this.heroesUrl)
+      .pipe(
+        tap(_ => this.log('fetch heroes')),
+        tap(heroes => this.log(`found ${heroes.length}`)),
+        catchError(this.handleError('getHeroes', []))
+      );
   }
 
+  /** Get hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send message after getting heroes
-    this.messageService.add(`HeroService: fetched Hero id = $(id)`);
-    return of(HEROES.find(hero => hero.id === id));
+    const url = `${this.heroesUrl}/${id}`;
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      tap(hero => this.log(`getting info for hero: ${hero.name}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
-  constructor(private messageService: MessageService) { }
+
+  /** Log a HeroService message with the Message Service */
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
+  }
+
+  private handleError<T> (operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // todo : send error to remote logging
+      console.error(error); // log to console for now
+
+      // TODO: bettter job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // let app keep running by returning an empy result
+      return of(result as T);
+    };
+  }
+
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+    ) { }
 }
